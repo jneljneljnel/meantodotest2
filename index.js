7d9f29c7e7d0b99ecaa2d9c4109b6944d1b5ed5c
jneljneljnel <whoisjeremynelson@gmail.com>
2015-08-30 16:24:16 -0500

    derp

--- index.js
+++ index.js
@@ -54,5 +54,5 @@ app.put('/tododb/:id', function (req, res) {
   );
 });
 
-// app.listen(3000);
-// console.log("Server running on port 3000");
+app.listen(3000);
+console.log("Server running on port 3000");
