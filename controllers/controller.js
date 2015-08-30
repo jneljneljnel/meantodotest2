51291cffad5412e49efa63267c588636e007ff4b
jneljneljnel <whoisjeremynelson@gmail.com>
2015-08-30 15:44:57 -0500

    idk anymore

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,11 +1,10 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-    console.log("Hello World from controller");
+
 
 
 var refresh = function() {
   $http.get('/tododb').success(function(response) {
-    console.log("got the data");
     $scope.tododb = response;
     $scope.todo = "";
   });
@@ -14,15 +13,13 @@ var refresh = function() {
 refresh();
 
 $scope.addTodo = function() {
-  console.log($scope.todo);
   $http.post('/tododb', $scope.todo).success(function(response) {
-    console.log(response);
+ 
     refresh();
   });
 };
 
 $scope.remove = function(id) {
-  console.log(id);
   $http.delete('/tododb/' + id).success(function(response) {
     refresh();
   });
--- server.js
+++ server.js
@@ -15,16 +15,15 @@ app.use(express.static(__dirname ));
 app.use(bodyParser.json());
 
 app.get('/tododb', function (req, res) {
-  console.log('I received a GET request');
+ 
 
   db.tododb.find(function (err, docs) {
-    console.log(docs);
+  
     res.json(docs);
   });
 });
 
-app.post('/tododb', function (req, res) {
-  console.log(req.body);
+app.post('/tododb', function (req, res) { 
   db.tododb.insert(req.body, function(err, doc) {
     res.json(doc);
   });
@@ -32,7 +31,6 @@ app.post('/tododb', function (req, res) {
 
 app.delete('/tododb/:id', function (req, res) {
   var id = req.params.id;
-  console.log(id);
   db.tododb.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
     res.json(doc);
   });
@@ -40,7 +38,6 @@ app.delete('/tododb/:id', function (req, res) {
 
 app.get('/tododb/:id', function (req, res) {
   var id = req.params.id;
-  console.log(id);
   db.tododb.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
     res.json(doc);
   });
@@ -48,7 +45,6 @@ app.get('/tododb/:id', function (req, res) {
 
 app.put('/tododb/:id', function (req, res) {
   var id = req.params.id;
-  console.log(req.body.name);
   db.tododb.findAndModify({
     query: {_id: mongojs.ObjectId(id)},
     update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
@@ -58,5 +54,5 @@ app.put('/tododb/:id', function (req, res) {
   );
 });
 
-// app.listen(3000);
-// console.log("Server running on port 3000");
+app.listen(3000);
+console.log("Server running on port 3000");
